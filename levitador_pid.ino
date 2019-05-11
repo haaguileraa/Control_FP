732bff22bbdde82d178de82fbdab62f72cbfb03c
Hernan Aguilera <haaguuileraa@unal.edu.co>
2019-05-10 19:43:22 -0500

    first commit

--- levitador_pid.ino
+++ levitador_pid.ino
@@ -1,35 +1,35 @@
 
 #include "TimerOne.h"
 
-//------------Constantes del Levitador-------------\\
 
-float ro = 1.2; //Kg/m^3
-float Cd = 20; //Segun ejemplo del profe
-float St = 1.96*pow(10,-5); // 3.14159265*(0.025)^2  //Area sec. transv.
-float alpha = (Cd*ro*St)/2;
+// constantes de la planta
 
+float cd = 0.8;
+float rho = 1.225;
+float radio = 3/1000;
+float m = 0.4/1000;
+float g = 9.8;
+float Area = 3.14*radio*radio;
+
+float alpha = 0.5*cd*rho*Area;
+float pp = alpha/m;
 
 // constantes del PID
 
-
+int n = 0;
 float u=0;
-float h=0, hn=0;
-int n=0;
+float hant=0, h=0, hn=0, hn2=0;
 int Fs = 200;
 float D = (float)1/Fs;
 long Ts =1000000/Fs; 
 float A = 3.14;
-float a = .5;
-float b = 1;
-float p1 = -D*a/A ;
-float p2 = D*b/A;
-float r = 1;
 float I=0, Iant=0;
 float eant=0;
 float DE=0;
-float kp = 2;
-float ki = 2;
-float kd = 2;
+float r = 1;
+float kp = 2000;
+float ki = 5000;
+float kd = 2000;
 float kib = ki*D;
 float kdb = kd/D;
 float e = 0;
@@ -53,15 +53,17 @@ void setup() {
 
 void control()
 {
-    n+=1;
 
-   if (n % 2500==0){
+  n+=1;
+
+ if (n % 2500==0){
     if (r <2){
       r = 2;
     }
     else
       r = 1;
    } 
+  
     // Control 
       
     
@@ -75,15 +77,16 @@ void control()
     eant = e; 
     
     // planta en lazo abierto
-   
-    hn = h + p1*sqrt(h) + p2*u; 
-    h = hn;
+    hn = h/(3.14*Fs) - hant/(3.14*Fs);
+    h = hant;
+    hn2 =  hn + pp*((u-hn)*(u-hn))/(3.14*Fs) -g/(3.14*Fs); 
+    hn = hn2;
  
-if (n % 5==0){
+ if (n % 5==0){
 
 Serial.print(r,3);
 Serial.print('\t');
-Serial.println(h,3);
+Serial.println(hn,3);
 } 
 
 }
